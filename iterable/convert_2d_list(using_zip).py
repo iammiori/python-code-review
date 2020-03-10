55feaed7c0a4c83fc434ca56c1f3e2a8b1e3dade
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2020-03-10 22:26:08 +0900

    code review-1

--- iterable/convert_2d_list(using_zip).py
+++ iterable/convert_2d_list(using_zip).py
@@ -8,3 +8,9 @@ def solution(mylist):
 input = [[1,2,3],[4,5,6],[7,8,9]]
 output = solution(input)
 print(output)
+
+# zip을 쓰지 않았다면 이중for문으로 값을 받아왔을것 같다.
+# 흔히 tranpose할때 유용할것 같다
+
+#---추가---
+# list comprehension과 map 함수를 비교해 봐야 겠다
